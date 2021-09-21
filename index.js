913db0ea6406c97fd7df3d9ea87767f2e79d58e4
HaarikaR <hramadugu@gmail.com>
2021-09-20 17:09:50 -0700

    Done

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
+  //this is where we can use conditionals given our argument, someValue
+  if(someValue <= 400){
+    return "This one is on me!";
+  } else if(someValue > 2000){
+    if(someValue >= 2500){
+return 'No can do.';
+    }
+    return 'I will gladly take your thirty bucks.';
+  }
+  }
+  //don't forget to return whatever the result is!
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.':'No go.';
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch(tip) {
+    case 'generous':
+      return"Thank you so much."
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return 'Bye.';
+    }
+  }
+  
